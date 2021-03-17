675de3cc22b968c82099da673b54439f82bf5631
Zach Gaddy <zachgaddy07@yahoo.com>
2021-03-17 11:07:01 -0700

    finished product

---
+++ 02-java-oop/03-zoo-keeper-pt1/ZooKeeper.java/src/com/zookeeper/projectone/GorillaTest.java
@@ -0,0 +1,16 @@
+package com.zookeeper.projectone;
+
+public class GorillaTest {
+
+	public static void main(String[] args) {
+		Gorilla g = new Gorilla();
+		g.throwSomething();
+		g.throwSomething();
+		g.throwSomething();
+		g.eatBananas();
+		g.eatBananas();
+		g.climb();
+		g.displayEnergy();
+	}
+
+}
