bdc030c37e0d308d90e20b2d4d5f9d9ef5ec1a77
Zach Gaddy <zachgaddy07@yahoo.com>
2021-03-23 20:58:05 -0700

    finished project

---
+++ 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/familiarrouting/src/main/java/com/zacharygaddy/familiarrouting/FamiliarroutingApplication.java
@@ -0,0 +1,14 @@
+package com.zacharygaddy.familiarrouting;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliarroutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamiliarroutingApplication.class, args);
+	}
+
+}
---
+++ 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/familiarrouting/src/test/java/com/zacharygaddy/familiarrouting/FamiliarroutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zacharygaddy.familiarrouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamiliarroutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
