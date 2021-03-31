bf19aca24a805d0b94ab56054cb6bfd9c675a561
Zach Gaddy <zachgaddy07@yahoo.com>
2021-03-30 21:06:15 -0700

    finished project

---
+++ 03-java-spring/01-spring-fundamentals/07-dojo-survey/DojoSurvey/src/test/java/com/zachgaddy/dojosurvey/DojoSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zachgaddy.dojosurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
